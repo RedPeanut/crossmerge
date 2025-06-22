8f267be955ed5a34b802ccf90ee595b7c44f7ecb
김진규 <zamong99@gmail.com>
2025-06-22 10:20:52 +0900

    update window menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -261,99 +261,174 @@ export default class MenuBuilder {
       subMenuView, subMenuWindow, subMenuHelp];
   }
 
-  buildDefaultTemplate() {
-    const templateDefault = [
-      {
-        label: '&File',
-        submenu: [
-          {
-            label: '&Open',
-            accelerator: 'Ctrl+O',
-          },
-          {
-            label: '&Close',
-            accelerator: 'Ctrl+W',
-            click: () => {
-              this.browserWindow.close();
-            },
-          },
-        ],
-      },
-      {
-        label: '&View',
-        submenu:
-          process.env.NODE_ENV === 'development' ||
-          process.env.DEBUG_PROD === 'true'
-            ? [
-                {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.browserWindow.webContents.reload();
-                  },
-                },
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.browserWindow.setFullScreen(
-                      !this.browserWindow.isFullScreen(),
-                    );
-                  },
-                },
-                {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.browserWindow.webContents.toggleDevTools();
-                  },
-                },
-              ]
-            : [
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.browserWindow.setFullScreen(
-                      !this.browserWindow.isFullScreen(),
-                    );
-                  },
-                },
-              ],
-      },
-      {
-        label: 'Help',
-        submenu: [
-          {
-            label: 'Learn More',
-            click() {
-              shell.openExternal('https://electronjs.org');
-            },
-          },
-          {
-            label: 'Documentation',
-            click() {
-              shell.openExternal(
-                'https://github.com/electron/electron/tree/main/docs#readme',
-              );
-            },
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://www.electronjs.org/community');
-            },
-          },
-          {
-            label: 'Search Issues',
-            click() {
-              shell.openExternal('https://github.com/electron/electron/issues');
-            },
-          },
-        ],
-      },
-    ];
+  buildDefaultTemplate(): MenuItemConstructorOptions[] {
+    const self = this;
 
-    return templateDefault;
+    const subMenuFile = {
+      label: '&File',
+      submenu: [
+        {
+          label: '&Open',
+          accelerator: 'Ctrl+O',
+        },
+        { type: 'separator' as const },
+        {
+          label: 'Preferences...',
+          accelerator: 'Ctrl+P',
+          click(item, focusedWindow) {
+            if(self.preferences && !self.preferences.isDestroyed()) {
+              self.preferences.show();
+            } else {
+              const preferences = self.preferences = new BrowserWindow({
+                parent: self.browserWindow, modal: false, show: false,
+                // titleBarStyle: 'hidden',
+                titleBarStyle: 'default',
+                title: 'Preferences',
+                // x, y, width, height,
+                webPreferences: {
+                  devTools: false,
+                  preload: app.isPackaged
+                    ? path.join(__dirname, 'preload.js')
+                    : path.join(__dirname, '../../.erb/dll/preload.js'),
+                },
+              });
+              // self.setupDevelopmentEnvironment(preferences);
+              preferences.loadURL(resolveHtmlPath('preferences.html'));
+              preferences.once('ready-to-show', () => {
+                preferences.show();
+              });
+            }
+          },
+        },
+        { type: 'separator' as const },
+        {
+          label: '&Close',
+          accelerator: 'Ctrl+W',
+          click: () => {
+            this.browserWindow.close();
+          },
+        },
+      ],
+    };
+
+    const subMenuMerging: MenuItemConstructorOptions = {
+      label: 'Merging',
+      submenu: [
+        { label: 'Current Change', submenu: [
+          { label: 'Push to Left', accelerator: 'Ctrl+Shift+Left', },
+          { label: 'Push to Right', accelerator: 'Ctrl+Shift+Right', }
+        ] },
+      ],
+    };
+
+    const subMenuActions: MenuItemConstructorOptions = {
+      label: 'Actions',
+      submenu: [
+        { label: 'Select Rows', submenu: [
+          { label: 'Select Changed', accelerator: 'Alt+Ctrl+C',
+            click(item, focusedWindow) {
+              mainWindow.send('menu click', { cmd: 'actions:select changed' });
+            }
+          }
+        ] },
+        { label: 'Expand All Folders', accelerator: 'Alt+Ctrl+=',
+          click(item, focusedWindow) {
+            mainWindow.send('menu click', { cmd: 'actions:expand all folders' });
+          }
+        },
+        { label: 'Collapse All Folders', accelerator: 'Alt+Ctrl+-',
+          click(item, focusedWindow) {
+            mainWindow.send('menu click', { cmd: 'actions:collapse all folders' });
+          }
+        },
+      ],
+    };
+
+    const subMenuViewDev: MenuItemConstructorOptions = {
+      label: '&View',
+      submenu: [
+        {
+          label: '&Reload',
+          accelerator: 'Ctrl+R',
+          click: () => {
+            this.browserWindow.webContents.reload();
+          },
+        },
+        {
+          label: 'Toggle &Full Screen',
+          accelerator: 'F11',
+          click: () => {
+            this.browserWindow.setFullScreen(
+              !this.browserWindow.isFullScreen(),
+            );
+          },
+        },
+        {
+          label: 'Toggle &Developer Tools',
+          accelerator: 'Alt+Ctrl+I',
+          click: () => {
+            this.browserWindow.webContents.toggleDevTools();
+          },
+        },
+      ]
+    };
+
+    const subMenuViewProd: MenuItemConstructorOptions = {
+      label: '&View',
+      submenu: [
+        {
+          label: 'Toggle &Full Screen',
+          accelerator: 'F11',
+          click: () => {
+            this.browserWindow.setFullScreen(
+              !this.browserWindow.isFullScreen(),
+            );
+          },
+        }
+      ]
+    };
+
+    const subMenuHelp: MenuItemConstructorOptions = {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() {
+            shell.openExternal('https://electronjs.org');
+          },
+        },
+        {
+          label: 'Documentation',
+          click() {
+            shell.openExternal(
+              'https://github.com/electron/electron/tree/main/docs#readme',
+            );
+          },
+        },
+        {
+          label: 'Community Discussions',
+          click() {
+            shell.openExternal('https://www.electronjs.org/community');
+          },
+        },
+        {
+          label: 'Search Issues',
+          click() {
+            shell.openExternal('https://github.com/electron/electron/issues');
+          },
+        },
+      ],
+    };
+
+    const subMenuView =
+    process.env.NODE_ENV === 'development' ||
+    process.env.DEBUG_PROD === 'true'
+      ? subMenuViewDev
+      : subMenuViewProd;
+
+    return [subMenuFile,
+      subMenuMerging, subMenuActions,
+      subMenuView, subMenuHelp];
   }
+
 }
