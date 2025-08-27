f0d824b11c599ae16164861900b58b88ef55655d
김진규 <zamong99@gmail.com>
2025-08-27 21:49:43 +0900

    .

--- src/renderer/util/FocusManager.ts
+++ src/renderer/util/FocusManager.ts
@@ -4,6 +4,10 @@ interface Item { what: HTMLElement, cb: (...args: any[]) => void }
  * 브라우저 포커스 처리와 좀 다르게 처리하기위해
  * = 등록되지 않은 엘리먼트로의 포커스 이동 무시를 위해
  * = Input 컴포넌트의 관련 리스트 항목 클릭이 가능하게 하기 위해
+ *
+ * To handle focus handling a bit differently from browser's
+ * = To ignore focus movement to unregistered elements
+ * = To enable click on related list items in Input components
  */
 export class FocusManager {
 
