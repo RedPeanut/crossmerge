52545b8207936f995bdc011733b4653e05341a8d
김진규 <zamong99@gmail.com>
2025-11-14 22:59:26 +0900

    add position, charset element in statusbar

--- src/renderer/part/StatusbarPart.ts
+++ src/renderer/part/StatusbarPart.ts
@@ -11,7 +11,11 @@ export interface StatusbarPartService {
 
 export class StatusbarPart extends Part implements StatusbarPartService {
 
-  span: HTMLElement;
+  // span: HTMLElement;
+  uid: string;
+  status: HTMLElement;
+  position: HTMLElement;
+  charset: HTMLElement;
 
   constructor(parent: HTMLElement, id: string, role: string, classes: string[], options: object) {
     super(parent, id, role, classes, options);
@@ -22,20 +26,48 @@ export class StatusbarPart extends Part implements StatusbarPartService {
 
   override createContentArea(): HTMLElement {
     const container: HTMLElement = super.createContentArea();
-    const span = this.span = $('span');
-    container.appendChild(span);
+    // const span = this.span = $('span');
+    // container.appendChild(span);
+
+    /*
+    <div class='status'></div>
+    <div class='position'></div>
+    <div class='charset'></div>
+    */
+
+    const status = this.status = $('div.status');
+    status.title = '';
+    const position = this.position = $('div.position');
+    position.title = 'Go to Line/Column';
+    position.innerHTML = 'Ln NN, Col NN';
+    // position.style.display = 'none';
+    const charset = this.charset = $('div.charset');
+    charset.title = 'Reopen with Encoding';
+    charset.innerHTML = 'UTF-8';
+    // charset.style.display = 'none';
+    charset.addEventListener('click', async (e) => {
+    });
+    container.appendChild(status);
+    container.appendChild(position);
+    container.appendChild(charset);
     return container;
   }
 
   update(item: CompareItem): void {
-    if(item.type === 'file')
-      this.span.textContent = `${item.status.removal} removalㆍ${item.status.insertion} insertionㆍ${item.status.change} change`;
-    else if(item.type === 'folder')
-      this.span.textContent = `${item.status.removed} removedㆍ${item.status.inserted} insertedㆍ${item.status.changed} changedㆍ${item.status.unchanged} unchanged`;
+    this.uid = item.uid;
+    if(item.type === 'file') {
+      this.status.innerHTML = `${item.status.removal} removalㆍ${item.status.insertion} insertionㆍ${item.status.change} change`;
+      this.position.style.display = 'block';
+      this.charset.style.display = 'block';
+    } else if(item.type === 'folder') {
+      this.status.innerHTML = `${item.status.removed} removedㆍ${item.status.inserted} insertedㆍ${item.status.changed} changedㆍ${item.status.unchanged} unchanged`;
+      this.position.style.display = 'none';
+      this.charset.style.display = 'none';
+    }
   }
 
   clear(): void {
-    this.span.textContent = ``;
+    this.status.innerHTML = ``;
   }
 
   /* layoutContainer(offset: number): void {
