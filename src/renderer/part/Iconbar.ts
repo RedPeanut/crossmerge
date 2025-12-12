dc9314b0ae243eb452a6c75961aedee176e0659d
김진규 <zamong99@gmail.com>
2025-12-12 14:22:05 +0900

    (iconbar) add dropdown mark, add fileview btn

--- src/renderer/part/Iconbar.ts
+++ src/renderer/part/Iconbar.ts
@@ -15,68 +15,166 @@ export class Iconbar implements IconbarService {
   }
 
   install(): void {
+    let group: HTMLElement, centered: HTMLElement;
+    let wrapBtn: HTMLElement, btnImg: HTMLElement, downBtnImg: HTMLElement;
+    let label: HTMLElement, separator: HTMLElement;
 
-    let group: HTMLElement, wrap: HTMLElement, btn: HTMLElement, label: HTMLElement;
-
+    // Common
     group = $('.group');
-    wrap = $('.wrap');
-    label = $('.label');
-    label.innerHTML = 'New';
+    group.style.display = 'block';
 
-    // const fileCompareBtn = $('a.material-symbols-outlined', null, 'article');
-    btn = $('a.codicon.codicon-file');
-    btn.addEventListener('click', (e: MouseEvent) => {
+    centered = $('.centered');
+
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {
       // console.log('fileCompareBtn is clicked ..');
       const bodyLayoutService = getService(bodyLayoutServiceId) as BodyLayoutService;
       bodyLayoutService.addFileCompareView();
     });
-    wrap.appendChild(btn);
-    // const folderCompareBtn = $('a.material-symbols-outlined', null, 'folder');
-    const folderCompareBtn = $('a.codicon.codicon-folder');
-    folderCompareBtn.addEventListener('click', (e: MouseEvent) => {
+    // const fileCompareBtn = $('a.material-symbols-outlined', null, 'article');
+    btnImg = $('a.codicon.codicon-file');
+    wrapBtn.appendChild(btnImg);
+    centered.appendChild(wrapBtn);
+
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {
       // console.log('folderCompareBtn is clicked ..');
       const bodyLayoutService = getService(bodyLayoutServiceId) as BodyLayoutService;
       bodyLayoutService.addFolderCompareView();
     });
-    wrap.appendChild(folderCompareBtn);
-    group.appendChild(wrap);
+    // const folderCompareBtn = $('a.material-symbols-outlined', null, 'folder');
+    btnImg = $('a.codicon.codicon-folder');
+    wrapBtn.appendChild(btnImg);
+    centered.appendChild(wrapBtn);
+
+    group.appendChild(centered);
+
+    label = $('.label');
+    label.innerHTML = 'New';
     group.appendChild(label);
+    separator = $('.separator');
+    group.appendChild(separator);
     this.container.appendChild(group);
 
+    // FolderView & FileView
     group = $('.group');
-    wrap = $('.wrap');
-    btn = $('a.codicon.codicon-debug-restart');
-    wrap.appendChild(btn);
-    btn = $('a.codicon.codicon-stop-circle');
+    centered = $('.centered');
+    btnImg = $('a.codicon.codicon-debug-restart');
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.appendChild(btnImg);
+    centered.appendChild(wrapBtn);
+
+    btnImg = $('a.codicon.codicon-stop-circle');
     // btn = $('a.codicon.codicon-close');
-    wrap.appendChild(btn);
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.appendChild(btnImg);
+    centered.appendChild(wrapBtn);
+
     label = $('.label');
     label.innerHTML = 'Start/Stop';
 
-    group.appendChild(wrap);
+    group.appendChild(centered);
     group.appendChild(label);
+    separator = $('.separator');
+    group.appendChild(separator);
     this.container.appendChild(group);
 
-    group = $('.group');
-    wrap = $('.wrap');
-    btn = $('a.codicon.codicon-code');
-    wrap.appendChild(btn);
+    /*
+    <div class="group">
+      <div class="centered">
+        <div class="wrap-btn">
+          <a class="codicon-xxx"></a>
+          <a class="codicon-chevron-down"></a>
+        </div>
+      </div class="label"></div>
+    </div>
+    */
+
+    // FolderView
+    group = $('.group.dropdown');
+    centered = $('.centered');
+    btnImg = $('a.codicon.codicon-code');
+    downBtnImg = $('a.codicon.codicon-chevron-down');
+
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {});
+    wrapBtn.appendChild(btnImg);
+    wrapBtn.appendChild(downBtnImg);
+    centered.appendChild(wrapBtn);
+
     label = $('.label');
     label.innerHTML = 'Copy Selected';
 
-    group.appendChild(wrap);
+    group.appendChild(centered);
     group.appendChild(label);
+    separator = $('.separator');
+    group.appendChild(separator);
     this.container.appendChild(group);
 
-    group = $('.group');
-    wrap = $('.wrap');
-    btn = $('a.codicon.codicon-list-selection');
-    wrap.appendChild(btn);
+    group = $('.group.dropdown');
+    centered = $('.centered');
+    btnImg = $('a.codicon.codicon-list-selection');
+    downBtnImg = $('a.codicon.codicon-chevron-down');
+
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {});
+    wrapBtn.appendChild(btnImg);
+    wrapBtn.appendChild(downBtnImg);
+    centered.appendChild(wrapBtn);
+
     label = $('.label');
     label.innerHTML = 'Select Rows';
 
-    group.appendChild(wrap);
+    group.appendChild(centered);
     group.appendChild(label);
+    separator = $('.separator');
+    group.appendChild(separator);
+    this.container.appendChild(group);
+
+    // FileView
+    group = $('.group');
+    centered = $('.centered');
+    btnImg = $('a.codicon.codicon-download');
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {});
+    wrapBtn.appendChild(btnImg);
+    centered.appendChild(wrapBtn);
+
+    btnImg = $('a.codicon.codicon-download.fliped');
+    // btn = $('a.codicon.codicon-close');
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {});
+    wrapBtn.appendChild(btnImg);
+    centered.appendChild(wrapBtn);
+
+    group.appendChild(centered);
+
+    label = $('.label');
+    label.innerHTML = 'Changes';
+    group.appendChild(label);
+    separator = $('.separator');
+    group.appendChild(separator);
+
+    this.container.appendChild(group);
+
+    group = $('.group.dropdown');
+    centered = $('.centered');
+    btnImg = $('a.codicon.codicon-code');
+    downBtnImg = $('a.codicon.codicon-chevron-down');
+
+    wrapBtn = $('.wrap-btn');
+    wrapBtn.addEventListener('click', (e: MouseEvent) => {});
+    wrapBtn.appendChild(btnImg);
+    wrapBtn.appendChild(downBtnImg);
+    centered.appendChild(wrapBtn);
+    group.appendChild(centered);
+
+    label = $('.label');
+    label.innerHTML = 'Current Change';
+    group.appendChild(label);
+    separator = $('.separator');
+    group.appendChild(separator);
+
     this.container.appendChild(group);
   }
 }
